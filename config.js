746832b573a643e9b210722dea97b64982c54073
fyomell <zahratusyita9834@gmail.com>
2025-09-13 14:04:47 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+// config.js
+const config = {
+  // Domain Pterodactyl kamu, JANGAN PAKE "/" di akhir
+  domain: "https://rofiksoleh.cloud-hosting.biz.id",
+
+  // Apikey dari Pterodactyl (Application API)
+  // Cara buat: Admin Area -> Application API -> Create New
+  apiKey: "ptla_Js4fy0YPY0e0nNpgjUPmCjiZwbBVFC32Y4QwCqqlIRl",
+
+  // Default ID untuk Egg, Nest, dan Location
+  eggId: "15",    // Ganti dengan ID Egg yang kamu mau (misal: NodeJS)
+  nestId: "5",     // Ganti dengan ID Nest dari Egg di atas
+  locationId: "1" // Ganti dengan ID Location server
+};
+
+export default config;
