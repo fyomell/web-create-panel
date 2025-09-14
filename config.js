72b4415e081f59f51b3cb0cdf033ff6576d6f473
fyomell <zahratusyita9834@gmail.com>
2025-09-15 00:33:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,7 @@
 // config.js
-
-const pterodactylConfig = {
+module.exports = {
   domain: "https://rofiksoleh.cloud-hosting.biz.id",
   apiKey: "ptla_REyLUYSXyLS6BE0Up3YSyd3fTV52mMQnjs07cFU0uDZ",
   eggId: "15",
-  nestId: "5",
   locationId: "1"
-};
-
-const telegramConfig = {
-  botToken: "KOSONGKAN_SAJA",
-  chatId: "KOSONGKAN_SAJA"
-};
-
-// Pake module.exports
-module.exports = {
-  pterodactylConfig,
-  telegramConfig
 };
