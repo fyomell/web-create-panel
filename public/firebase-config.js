c3b1664196651c4461fcded5e7953334d8b8929c
fyomell <zahratusyita9834@gmail.com>
2025-09-15 01:40:55 +0700

    Update public/firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -5,12 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/9.15.0/firebase
 
 // PASTE KUNCI FIREBASE KAMU DI SINI
 const firebaseConfig = {
-  apiKey: "AIzaSy...",
-  authDomain: "proje...",
-  projectId: "proje...",
-  storageBucket: "proje...",
-  messagingSenderId: "...",
-  appId: "1:..."
+  apiKey: "AIzaSyDZnXCAO6b7mu5E3lC1xGEO_YUByphQB2k",
+  authDomain: "web-gw-f63be.firebaseapp.com",
+  projectId: "web-gw-f63be",
+  storageBucket: "web-gw-f63be.firebasestorage.app",
+  messagingSenderId: "839331123143",
+  appId: "1:839331123143:web:defa080f403bf1ebd953e2",
+  measurementId: "G-FK4RH5ZQY2"
 };
 
 const app = initializeApp(firebaseConfig);
