b12fbc9a97a789af84ed4648c20a80d70d7bd1f1
fyomell <zahratusyita9834@gmail.com>
2025-09-15 01:57:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@
 module.exports = {
   pterodactylConfig: {
     domain: "https://rofiksoleh.cloud-hosting.biz.id",
-    apiKey: "ptla_REyLUYSXyLS6BE0Up3YSyd3fTV52mMQnjs07cFU0uDZ",
+    apiKey: "ptla_WIO5duQxqL7EfeVAIxaSBQ0vnLXBereXMuLbn5ItvH2",
     eggId: "15",
     nestId: "5",
     locationId: "1"
   },
   githubConfig: {
-    username: "NAMA_USER_GITHUB_KAMU",
+    username: "fyomell",
     repoName: "reseller-data", // Nama repo private-mu
   }
 };
