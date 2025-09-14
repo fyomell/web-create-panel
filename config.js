20b8916ad8b8daed4ef1fbfc6e7ab88e990f2a95
fyomell <zahratusyita9834@gmail.com>
2025-09-15 00:37:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,13 @@
 // config.js
 module.exports = {
+  // Domain Pterodactyl kamu
   domain: "https://rofiksoleh.cloud-hosting.biz.id",
+
+  // Apikey dari Pterodactyl
   apiKey: "ptla_REyLUYSXyLS6BE0Up3YSyd3fTV52mMQnjs07cFU0uDZ",
+
+  // Default ID untuk Egg, Nest, dan Location
   eggId: "15",
+  nestId: "5", // <-- INI DIA, UDAH BALIK LAGI
   locationId: "1"
 };
