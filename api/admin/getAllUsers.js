f65e4c369fd5374ed5ec680b1ca3fac0319f8544
fyomell <zahratusyita9834@gmail.com>
2025-09-14 23:59:19 +0700

    Update api/admin/getAllUsers.js

--- api/admin/getAllUsers.js
+++ api/admin/getAllUsers.js
@@ -1,19 +1,17 @@
 // api/admin/getAllUsers.js
-import { db, verifyUser } from '../_firebase-admin.js';
+const { db, verifyUser } = require('../_firebase-admin.js');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
     try {
-        await verifyUser(req, 'owner'); // Hanya owner yang bisa akses
-        
+        await verifyUser(req, 'owner');
         const usersSnapshot = await db.collection('users').get();
         const users = usersSnapshot.docs.map(doc => {
             const data = doc.data();
             delete data.email; 
             return { id: doc.id, ...data };
         });
-
         res.status(200).json(users);
     } catch (error) {
         res.status(403).json({ message: error.message });
     }
-}
+};
