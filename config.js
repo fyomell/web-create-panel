8a9bf1cdf622204a98d98a6ee35a2ec7df0128b7
fyomell <zahratusyita9834@gmail.com>
2025-09-14 23:04:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,21 @@
 // config.js
-const config = {
+
+// Pastikan ada tulisan 'export' di sini
+export const pterodactylConfig = {
   // Domain Pterodactyl kamu, JANGAN PAKE "/" di akhir
   domain: "https://rofiksoleh.cloud-hosting.biz.id",
 
   // Apikey dari Pterodactyl (Application API)
-  // Cara buat: Admin Area -> Application API -> Create New
-  apiKey: "ptla_REyLUYSXyLS6BE0Up3YSyd3fTVS2mMQnjsO7cFU0uDZ",
+  apiKey: "ptla_REyLUYSXyLS6BE0Up3YSyd3fTV52mMQnjs07cFU0uDZ",
 
   // Default ID untuk Egg, Nest, dan Location
-  eggId: "15",    // Ganti dengan ID Egg yang kamu mau (misal: NodeJS)
-  nestId: "5",     // Ganti dengan ID Nest dari Egg di atas
-  locationId: "1" // Ganti dengan ID Location server
+  eggId: "15",
+  nestId: "5",
+  locationId: "1"
 };
 
-export default config;
+// Biarkan ini ada untuk masa depan, tapi tidak akan dipakai sekarang
+export const telegramConfig = {
+  botToken: "KOSONGKAN_SAJA",
+  chatId: "KOSONGKAN_SAJA"
+};
